e3fec1234e58133b2b06a88bb1746e005ac95743
DmitryKuprienko <101987031+DmitryKuprienko@users.noreply.github.com>
2022-11-11 14:33:35 +0000

    Deploying to gh-pages from @ DmitryKuprienko/goit-js-hw-09@c4dc5201cf906f09e3cb2417416da0b301c65f35 🚀

---
+++ 02-timer.ea04f835.js
@@ -0,0 +1,2 @@
+!function(){var o;console.log((o=".timer",document.querySelector(o)));var e=Date.now();console.log(e),setInterval((function(){var o=Date.now();console.log(o)}),1e4)}();
+//# sourceMappingURL=02-timer.ea04f835.js.map
