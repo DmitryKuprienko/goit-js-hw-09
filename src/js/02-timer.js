fa9ee9baf1ce2e1f8cf4bec61eeda58ef7b78dba
DmitryKuprienko <101987031+DmitryKuprienko@users.noreply.github.com>
2022-11-14 11:29:48 +0200

    ggg

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -10,16 +10,3 @@ const element= x =>document.querySelector(x);
 //     console.log(date2)
 // }
 //     ,10000)
-const timer = {
-    const starttimer = Date.now();
-start{   
-    setInterval(()=>{
-    
-
-    console.log("час", starttimer)},1000)
-        
-   }}
-
-
-    
-}
